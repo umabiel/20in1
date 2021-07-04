f3d73c97f2f5abee0e7db1525f5faadc33727e34
Gustavo I. Chalco <gchalcoa@gmail.com>
2021-07-03 21:58:18 -0500

    Se utiliza libreria classnames para mejor manejo de clases css dinamicas y arreglos generales

--- src/components/QuoteGenerator/QuoteGenerator.tsx
+++ src/components/QuoteGenerator/QuoteGenerator.tsx
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from "axios";
+import classNames from "classnames";
 import React, { Fragment, useEffect, useState } from "react";
 
 import "../../App.scss";
@@ -16,49 +17,59 @@ export const QuoteGenerator = () => {
 
   const [quotes, setQuotes] = useState<Quote[]>(initialState);
   const [quote, setQuote] = useState<Quote>({} as Quote);
+  const [loading, setLoading] = useState(true);
+
+  const classes = classNames({
+    loader: true,
+    show: loading,
+  });
+  const classesText = classNames("quote-text", { "quote-text-show": !loading });
 
   useEffect(() => {
     axios.get(API_URL).then((res: AxiosResponse<Quote[]>) => {
+      setQuote(res.data.pop()!);
       setQuotes(res.data);
-      console.log(quotes);
+      setLoading(false);
     });
     return () => {};
   }, []);
 
   const getQuoteFromMemory = () => {
-    console.log("mas");
     if (quotes.length >= 0) {
-      console.log("Antes...", quotes.length);
       const q: Quote = quotes.pop()!;
-      setQuotes({
-        ...quotes,
-      });
-      console.log("despues...", quotes.length);
+      setQuotes(quotes);
       setQuote({
         ...q,
       });
     }
   };
 
+  const tweetQuote = () => {
+    const tq = quote;
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${tq.text} - ${tq.author}`;
+    window.open(tweetUrl, "_blank");
+  };
+
   return (
     <Fragment>
       <div className="quote-container" id="quote-container">
+        <div className={classes} id="loader"></div>
         {/* Quote */}
-        <div className="quote-text">
+        <div className={classesText}>
           <i className="fa fa-quote-left"></i>
-          <span id="qoute">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur
-            rerum assumenda iure repellat, dolor impedit atque odit itaque
-            adipisci fugit ullam eaque perferendis veniam doloribus ipsa
-            sapiente ad nisi dignissimos!
-          </span>
+          <span id="qoute">{quote.text}</span>
           {/* Author */}
           <div className="quote-author">
-            <span id="author">DF</span>
+            <span id="author">{quote.author}</span>
           </div>
           {/* Buttons */}
           <div className="button-container">
-            <button className="twitter-button" id="twitter" title="Tweet me!">
+            <button
+              className="twitter-button"
+              onClick={tweetQuote}
+              id="twitter"
+              title="Tweet me!"
+            >
               <i className="fa fa-twitter"></i>
             </button>
             <button id="new-qoute" onClick={getQuoteFromMemory}>
